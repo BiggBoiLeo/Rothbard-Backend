62dbf556cad6825eee11b1e209a0cbe1028dd7a5
BiggBoiLeo <leojohnsherman@gmail.com>
2024-09-30 12:28:12 -0400

    prevent clickjacking

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Apply security headers and middleware
-app.use(helmet.frameguard({ action: 'sameorigin'}));
+app.use(helmet.frameguard({ action: 'DENY'}));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
