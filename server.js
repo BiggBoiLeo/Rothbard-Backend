f235b48758e652f9a283e88d88ee9a4ff34cfc8c
BiggBoiLeo <leojohnsherman@gmail.com>
2024-09-30 14:22:27 -0400

    add helmet frameguard and contentSecurityPolicy

--- server.js
+++ server.js
@@ -19,12 +19,14 @@ app.use(helmet());
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-// Custom middleware to set security headers for clickjacking protection
-app.use((req, res, next) => {
-    res.setHeader("X-Frame-Options", "DENY");
-    res.setHeader("Content-Security-Policy", "frame-ancestors 'none'");
-    next();
-});
+// Set security headers for clickjacking protection using Helmet
+app.use(helmet.frameguard({ action: 'deny' })); // Same as setting X-Frame-Options to DENY
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        frameAncestors: ["'none'"], // Same as setting frame-ancestors to 'none'
+    },
+}));
 
 
 // CORS middleware
