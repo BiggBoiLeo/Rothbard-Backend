13efd1beafa7de3000b0bc2968b8ae6f1b724dae
BiggBoiLeo <leojohnsherman@gmail.com>
2024-10-01 13:08:01 -0400

    cleanup for clickjacking

--- server.js
+++ server.js
@@ -25,17 +25,6 @@ app.use(helmet());
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-// Set custom security headers for clickjacking protection
-
-// Set security headers for clickjacking protection using Helmet
-app.use(helmet.frameguard({ action: 'deny' })); // Same as setting X-Frame-Options to DENY
-app.use(helmet.contentSecurityPolicy({
-    directives: {
-        defaultSrc: ["'self'"],
-        frameAncestors: ["'none'"], // Same as setting frame-ancestors to 'none'
-    },
-}));
-
 
 // CORS middleware
 app.use(corsMiddleware);
